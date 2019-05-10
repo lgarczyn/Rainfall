e0b8e4fe7faf27f6d6d14dd7b2e1962d8758bdcf
lgarczyn <lgarczyn@student.42.fr>
2019-05-10 20:21:22 +0200

    finished source.cpp

--- level9/source.cpp
+++ level9/source.cpp
@@ -1,20 +1,33 @@
 #include <cstring>
+#include <cstdlib>
 
 class Number
 {
   private:
-	int _n;
 	char _annotation[100];
+	int _n;
   public:
 	Number(int n):_n(n) {}
 	void setAnnotation(const char *str)
 	{
 		memcpy(_annotation, str, strlen(str));
 	}
+	int operator+(Number& arg)
+	{
+		return (this->_n + arg._n);
+	}
+	int operator-(Number& arg)
+	{
+		return (this->_n - arg._n);
+	}
 };
 
-
-int main(void)
+int main(int ac, char **av)
 {
-	
+	if (ac != 2)	
+		std::exit(1);
+	Number *num1 = new Number(5);
+	Number *num2 = new Number(6);
+	num1->setAnnotation(av[1]);
+	return (*num1 + *num2);
 }
